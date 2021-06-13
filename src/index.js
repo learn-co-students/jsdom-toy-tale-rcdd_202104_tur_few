5f06837bbb3b118fc0717295db20b8ceaf705a0a
Kutay Kagan Ozen <kutay.kagan.ozenn@gmail.com>
2021-06-13 15:00:33 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -13,3 +13,36 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+fetch ("http://localhost:3000/toys")
+.then(response => response.json())
+.then(json => addToys(json));
+console.log(json)
+const collection = document.getElementById('toy-collection');
+function addToys(data) {
+  data.forEach(element => {
+    const card = document.createElement('div')
+    const name = document.createElement('h2')
+    const img = document.createElement('img')
+    const likes = document.createElement('p')
+    const button = document.createElement('button')
+
+    card.setAttribute('class', 'card')
+    name.innerText = element.name;
+    img.setAttribute('src', element.image)
+    img.setAttribute('class', 'toy-avatar')
+    likes.innerText = element.likes + 'likes'
+    button.setAttribute('class', 'like-btn')
+
+    card.append(name)
+    card.append(img)
+    card.append(likes)
+    card.append(button)
+
+    collection.append(card)
+
+
+  });
+}
+
+//ill finish this later
