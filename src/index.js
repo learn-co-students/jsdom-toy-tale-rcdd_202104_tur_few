7b3b4d5383f13b096d50832515a99ea04738df5f
Ali Rıza <ars.lifestyle@hotmail.com>
2021-06-20 17:40:25 +0300

    Fetch, update the DOM, create cards and add the like functionality.

--- src/index.js
+++ src/index.js
@@ -13,3 +13,49 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+let togglerForTheLikeButton = false;
+let BASE_URL = "http://localhost:3000" // it has /toys in it 
+
+fetch(BASE_URL + "/toys")
+.then((response) => response.json())
+.then((json) => {
+  console.log(json)
+  let toyCollectionDiv = document.getElementById("toy-collection");
+  json.forEach((toyObj) => {
+    let divCard = document.createElement("div");
+    divCard.setAttribute("class", "card");
+
+    let name = document.createElement("h2");
+    name.innerText = toyObj.name;
+
+    let img = document.createElement("img");
+    img.src = toyObj.image;
+    img.setAttribute("class", "toy-avatar");
+ 
+    let likes = document.createElement("p");
+    likes.innerText = toyObj.likes;
+
+    let likeButton = document.createElement("button");
+    likeButton.innerText = "Like!";
+    likeButton.addEventListener("click", () => {
+      if(togglerForTheLikeButton === false) {
+        likes.innerText = parseInt(likes.innerText) + 1;
+      }
+      else if (togglerForTheLikeButton === true) {
+        likes.innerText = parseInt(likes.innerText) - 1;
+      }
+      togglerForTheLikeButton = !togglerForTheLikeButton 
+    })
+
+    divCard.appendChild(name);
+    divCard.appendChild(img);
+    divCard.appendChild(likes);
+    divCard.appendChild(likeButton);
+
+    toyCollectionDiv.appendChild(divCard);
+  })
+
+
+})
