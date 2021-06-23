0fa7aaf52468edd120d0e7c54218d89838e41ec0
Ali Rıza <ars.lifestyle@hotmail.com>
2021-06-23 14:19:41 +0300

    Final product.

--- src/index.js
+++ src/index.js
@@ -18,11 +18,11 @@ document.addEventListener("DOMContentLoaded", () => {
 let togglerForTheLikeButton = false;
 let BASE_URL = "http://localhost:3000" // it has /toys in it 
 
-fetch(BASE_URL + "/toys")
+fetch(BASE_URL + "/toys") // get all the toys and display them in the DOM
 .then((response) => response.json())
 .then((json) => {
-  console.log(json)
   let toyCollectionDiv = document.getElementById("toy-collection");
+  let buttonIdCounter = 0;
   json.forEach((toyObj) => {
     let divCard = document.createElement("div");
     divCard.setAttribute("class", "card");
@@ -38,15 +38,26 @@ fetch(BASE_URL + "/toys")
     likes.innerText = toyObj.likes;
 
     let likeButton = document.createElement("button");
+    likeButton.id = ++buttonIdCounter; 
     likeButton.innerText = "Like!";
-    likeButton.addEventListener("click", () => {
+    likeButton.addEventListener("click", (e) => {
+      e.preventDefault()
+      fetch(`${BASE_URL}/toys/${e.target.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({likes: togglerForTheLikeButton ? parseInt(likes.innerText) - 1 : parseInt(likes.innerText) + 1})
+      })
+      
       if(togglerForTheLikeButton === false) {
         likes.innerText = parseInt(likes.innerText) + 1;
       }
       else if (togglerForTheLikeButton === true) {
         likes.innerText = parseInt(likes.innerText) - 1;
       }
-      togglerForTheLikeButton = !togglerForTheLikeButton 
+      togglerForTheLikeButton = !togglerForTheLikeButton
     })
 
     divCard.appendChild(name);
@@ -56,6 +67,20 @@ fetch(BASE_URL + "/toys")
 
     toyCollectionDiv.appendChild(divCard);
   })
+})
 
+let form = document.querySelector(".add-toy-form");
+let nameInput = document.getElementById("name");
+let imgURLInput = document.getElementById("imgURL");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  fetch(BASE_URL + "/toys", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({ name: nameInput.nodeValue, image: imgURLInput, likes: 0})
+  }).then(resp => resp.json()).then(json => console.log(json))
 })
